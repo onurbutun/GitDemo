86acebf73342e84ac34622933b2ba6904daa05f7
onurbutun <doganonurbutun@gmail.com>
2019-04-29 17:34:42 +0300

    first commit

---
+++ src/test/java/qaclickacademy/RESTAPITest.java
@@ -0,0 +1,15 @@
+package qaclickacademy;
+
+import org.testng.annotations.Test;
+
+public class RESTAPITest {
+
+	@Test
+	public void PostJIRA() {
+		System.out.println("PostJIRA");
+	}
+	@Test
+	public void DeleteTwitter() {
+		System.out.println("DeleteTwitter");
+	}
+}
